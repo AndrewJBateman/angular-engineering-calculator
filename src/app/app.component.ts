0c870483f6ce1a0a5c279e6a368e14a24172518c
Andrew Bateman <gomezbateman@yahoo.com>
2023-01-10 15:38:29 +0100

    chore: build folder removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<app-calculator></app-calculator>`
 })
 export class AppComponent {
   title = 'angular-engineering-calculator';  
